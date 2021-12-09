02254f9643bd081f911fb62710568da4f4806695
Emine Dogan <emine.dogan@gmail.com>
2021-12-08 21:32:38 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+
+
+# Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+# Write one line of Python that takes this list a and
+# makes a new list that has only the even elements of this list in it.
+
+def even_elements():
+    a = [1,4,9,16,25,36,49,64,81,100]
+
+# one line solution
+    return [ i for i in a if i%2 ==0]
+
+
+
+
+
+
+#   even_a = []
+
+#   for i in a:
+#       if i%2 == 0:
+#           even_a.append(i)
+#   print (even_a)
+
+
+
+
+
+
+if __name__ == '__main__':
+    print(even_elements())
+
+
